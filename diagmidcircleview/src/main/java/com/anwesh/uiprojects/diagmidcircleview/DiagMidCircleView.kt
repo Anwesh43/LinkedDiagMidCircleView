2ade3e4b3d50013f79dc77c214d93af1e555c02f
Anwesh43 <anweshthecool0@gmail.com>
2019-04-23 01:37:12 +0530

    adding libraries and constants

--- diagmidcircleview/src/main/java/com/anwesh/uiprojects/diagmidcircleview/DiagMidCircleView.kt
+++ diagmidcircleview/src/main/java/com/anwesh/uiprojects/diagmidcircleview/DiagMidCircleView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.diagmidcircleview
 /**
  * Created by anweshmishra on 23/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+

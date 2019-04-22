0e87986a25ef948bca41c6de9f5aa3eefe53bb0d
Anwesh43 <anweshthecool0@gmail.com>
2019-04-23 02:13:53 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkeddiagmidcircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddiagmidcircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkeddiagmidcircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.diagmidcircleview.DiagMidCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DiagMidCircleView.create(this)
     }
 }

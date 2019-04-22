cb2cecc5dc528fb5da6bd95fe25ae0c93a811a92
Anwesh43 <anweshthecool0@gmail.com>
2019-04-23 02:03:47 +0530

    adding DMCNode class

--- diagmidcircleview/src/main/java/com/anwesh/uiprojects/diagmidcircleview/DiagMidCircleView.kt
+++ diagmidcircleview/src/main/java/com/anwesh/uiprojects/diagmidcircleview/DiagMidCircleView.kt
@@ -126,4 +126,48 @@ class DiagMidCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DMCNode(var i : Int, val state : State = State()) {
+
+        private var prev : DMCNode? = null
+        private var next : DMCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = DMCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDMCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DMCNode {
+            var curr : DMCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
